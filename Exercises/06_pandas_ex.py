b8c8269b2b190c1206a7754fea14696da3bba939
Guy_Weintraub <guywine@gmail.com>
2020-05-06 11:54:00 +0300

    ex 06_pandas

---
+++ Exercises/06_pandas_ex.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+cars = {'Tel Aviv': 123,
+        'Jerusalem': 115,
+        'Haifa': 80,
+        'Beer Sheva': 95,
+        'Rishon Lezion': 70,
+        'Netanya': 70,
+        'Petah Tikva': 62}
+
+population = {'Tel Aviv': 750.2,
+              'Jerusalem': 900.,
+              'Haifa': 700.9,
+              'Beer Sheva': 650.4,
+              'Rishon Lezion': 561.6,
+              'Netanya': 400.6,
+              'Petah Tikva': 390.1}
